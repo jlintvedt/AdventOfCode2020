a7c4cfcbc87082cbe7fc2b69618ba9c2b025ec47
jlintvedt <jlintvedt@gmail.com>
2020-12-02 06:54:45 +0100

    Benchmark Day02

---
+++ AdventOfCodeBenchmark/Day02Benchmark.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode;
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCodeBenchmark
+{
+    public class Day02Benchmark
+    {
+        string input;
+
+        [Params(100000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            input = AdventOfCodeTests.Resources.Input.D02_Puzzle;
+        }
+
+        [Benchmark]
+        public string D02_P1() => Day02.Puzzle1(input);
+
+        [Benchmark]
+        public string D02_P2() => Day02.Puzzle2(input);
+    }
+}
