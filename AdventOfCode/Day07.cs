a8e7101c2effeccd9a5ece9d7a1601a67b838d4c
jlintvedt <jlintvedt@gmail.com>
2020-12-07 06:49:18 +0100

    Solve Day07 Puzzle1

--- AdventOfCode/Day07.cs
+++ AdventOfCode/Day07.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -7,10 +8,94 @@ namespace AdventOfCode
     /// </summary>
     public class Day07
     {
+        public class BaggageProcessing
+        {
+            public Dictionary<string, BagType> Bags;
+
+            public BaggageProcessing(string[] rules)
+            {
+                Bags = new Dictionary<string, BagType>();
+
+                foreach (var rule in rules)
+                {
+                    var parts = rule.Split(" bags contain ");
+                    var bag = GetOrCreateBag(parts[0]);
+                    // Parse content
+                    if (parts[1] == "no other bags.")
+                    {
+                        continue;
+                    }
+                    foreach (var c in parts[1].Split(", "))
+                    {
+                        // Only handles single digit
+                        var num = Int32.Parse(c.Substring(0, 1));
+                        // Extract name without ' bags' ending
+                        var name = c.Substring(2).Split(" bag")[0];
+                        var cBag = GetOrCreateBag(name);
+                        // Link parent and child bag
+                        bag.Contains.Add(cBag, num);
+                        cBag.AppearIn.Add(bag);
+                    }
+                }
+            }
+
+            public int CalculateNumPossibleContainers(string name)
+            {
+                var visited = new HashSet<string>();
+                var bag = Bags[name];
+
+                var numVisited = RecursiveVisitParents(bag, visited) - 1;
+
+                return numVisited;
+            }
+
+            public int RecursiveVisitParents(BagType bag, HashSet<string> visited)
+            {
+                var numVisited = 1;
+                foreach (var parent in bag.AppearIn)
+                {
+                    if (visited.Contains(parent.Name))
+                    {
+                        continue;
+                    }
+                    numVisited += RecursiveVisitParents(parent, visited);
+                    visited.Add(parent.Name);
+                }
+                return numVisited;
+            }
+
+            private BagType GetOrCreateBag(string name)
+            {
+                if (Bags.ContainsKey(name))
+                {
+                    return Bags[name];
+                }
+                var bag = new BagType(name);
+                Bags.Add(name, bag);
+                return bag;
+            }
+        }
+
+        public class BagType
+        {
+            public string Name;
+            public List<BagType> AppearIn;
+            public Dictionary<BagType, int> Contains;
+
+            public BagType(string name)
+            {
+                Name = name;
+                AppearIn = new List<BagType>();
+                Contains = new Dictionary<BagType, int>();
+            }
+        }
+
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return input + "_Puzzle1";
+            var bp = new BaggageProcessing(input.Split(Environment.NewLine));
+            return bp.CalculateNumPossibleContainers("shiny gold").ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day07Tests.cs
+++ AdventOfCodeTests/Day07Tests.cs
@@ -15,7 +15,7 @@ namespace AdventOfCodeTests
         {
             string day = "07";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("light red bags contain 1 bright white bag, 2 muted yellow bags.{0}dark orange bags contain 3 bright white bags, 4 muted yellow bags.{0}bright white bags contain 1 shiny gold bag.{0}muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.{0}shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.{0}dark olive bags contain 3 faded blue bags, 4 dotted black bags.{0}vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.{0}faded blue bags contain no other bags.{0}dotted black bags contain no other bags.", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -32,7 +32,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day07.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"{input_puzzle}_Puzzle1", result);
+            Assert.AreEqual("229", result);
         }
 
         [TestMethod]
@@ -42,7 +42,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day07.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"{input_example1}_Puzzle1", result);
+            Assert.AreEqual("4", result);
         }
 
         [TestMethod]
