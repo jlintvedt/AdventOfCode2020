5f713c10b98717179f030c1dd9e3b95e46d68ea5
jlintvedt <jlintvedt@gmail.com>
2020-12-10 06:15:48 +0100

    Solve Day10 Puzzle1

--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -1,17 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode
 {
     /// <summary>
-    /// Template class
     /// https://adventofcode.com/2020/day/10
     /// </summary>
     public class Day10
     {
+        public class AdapterArray
+        {
+            private List<int> Adapters;
+
+            public AdapterArray(string rawInput)
+            {
+                Adapters = Common.Common.ParseStringToIntArray(rawInput, Environment.NewLine).ToList();
+                Adapters.Sort();
+                // Add Device
+                Adapters.Add(Adapters[Adapters.Count - 1] + 3);
+            }
+
+            public int ChainAdapters()
+            {
+                var diffOne = 0;
+                var diffThree = 0;
+                var jolt = 0;
+
+                foreach (var adapter in Adapters)
+                {
+                    var diff = adapter - jolt;
+                    if (diff == 1)
+                    {
+                        diffOne++;
+                    }
+                    else if (diff == 2) { }
+                    else if (diff == 3)
+                    {
+                        diffThree++;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid Jolt difference");
+                    }
+                    jolt = adapter;
+                }
+
+                return diffOne * diffThree;
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return input + "_Puzzle1";
+                var aa = new AdapterArray(input);
+                return aa.ChainAdapters().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day10Tests.cs
+++ AdventOfCodeTests/Day10Tests.cs
@@ -15,8 +15,8 @@ namespace AdventOfCodeTests
         {
             string day = "10";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
-            input_example2 = string.Format("example{0}2", Environment.NewLine);
+            input_example1 = string.Format("16{0}10{0}15{0}5{0}1{0}11{0}7{0}19{0}6{0}12{0}4", Environment.NewLine);
+            input_example2 = string.Format("28{0}33{0}18{0}42{0}31{0}14{0}46{0}20{0}48{0}47{0}24{0}23{0}49{0}45{0}19{0}38{0}39{0}11{0}1{0}32{0}25{0}35{0}8{0}17{0}7{0}9{0}4{0}2{0}34{0}10{0}3", Environment.NewLine);
         }
 
         [TestMethod]
@@ -32,17 +32,27 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day10.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"{input_puzzle}_Puzzle1", result);
+            Assert.AreEqual("1914", result);
         }
 
         [TestMethod]
-        public void Puzzle1_Example()
+        public void Puzzle1_Example1()
         {
             // Act
             var result = AdventOfCode.Day10.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"{input_example1}_Puzzle1", result);
+            Assert.AreEqual("35", result);
+        }
+
+        [TestMethod]
+        public void Puzzle1_Example2()
+        {
+            // Act
+            var result = AdventOfCode.Day10.Puzzle1(input_example2);
+
+            // Assert
+            Assert.AreEqual("220", result);
         }
 
         [TestMethod]
